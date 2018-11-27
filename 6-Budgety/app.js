e96dcab4ddf3cc29ff24fccd88cef0f8e10b419f
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2018-11-27 13:26:58 -0500

    ForEach method to process arrays and how to use Array.prototype.call

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -103,6 +103,24 @@ var UIController = (function() {
 
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            // querySelectorAll returns a 'LIST', so fields is not an array.             
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            // Converting List 'fields' into an array
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            // Modern method to loop Arrays
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -138,9 +156,12 @@ var controller = (function (budgetCtrl, UICtrl) {
             // 3. Add the item to the UI 
             UICtrl.addListItem(newItem, input.type);
 
-            // 4. Calculate the budget
+            // 4. Clear the Fields
+            UICtrl.clearFields();
 
-            // 5. Display the budget on the UI
+            // 5. Calculate the budget
+
+            // 6. Display the budget on the UI
     };
 
     return {
