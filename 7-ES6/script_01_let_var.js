3ad4938db9091393b3baa098cdff07fe9e057abe
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-07-02 23:42:57 -0500

    ES6 variables - let and const - Part 2 - let and const are block scoped, var is function scoped

--- 7-ES6/script_01_let_var.js
+++ 7-ES6/script_01_let_var.js
@@ -1,76 +1,54 @@
 // Lecture 01: let and const
 
-/*
+//  ES5
+// Function scoped
+// var name5 = 'Jane Smith';
+// var age5 = 23;
+// name5 = 'Jane Miller';
+// console.log(name5,age5);
+
+// // ES6
+// // Block scoped
+// const name6 = 'Jane Smith';
+// let age6 = 23;
+// // const values can not change
+// // name6 = 'Jane Miller'
+// console.log(name6,age6);
 
 // ES5
-var name5 = 'Jane Smith';
-var age5 = 23;
+function driverLicense5(passedTest) {
+    // var is Function-Scoped
+    if (passedTest) {
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+    console.log(firstName,'born in',yearOfBirth,',is now officially allowed to drive a car.');
+}
 
-name5 = 'Jane Miller';
-console.log(name5, age5);
-
-// ES6
-const name6 = 'Jane Smith';
-let age6 = 23;
-
-// name6 = 'Jane Miller'; // CONST have immutable values!
-console.log(name6, age6)
-*/
-
-// ES5 functions
-// function driverLicense6 (passedTest) {
-//     // VAR => Function Scoped
-//     if (passedTest) {
-//         console.log(firstName);
-//         var firstName = 'John';
-//         var yearOfBirth = 1990;
-//     }
-//     console.log(firstName + ', born on ' + yearOfBirth + ' is now oficially allowed to drive a car.');
-// }
-// driverLicense(true);
-
-// // ES6 functions
-// function driverLicense (passedTest) {
-//     // LET => Block scoped
-//     // console.log(firstName); with LET we can't use a variable before it is declared
-//     let firstName;
-//     const yearOfBirth = 1990;
-//     if (passedTest) {
-//         firstName = 'John';
-//     }
-//     console.log(firstName + ', born on ' + yearOfBirth + ' is now oficially allowed to drive a car.');
-// }
-// driverLicense6(true);
-
-// console.log('\n')
-// // ES5
-// // Here i is the same variable, children functions can access parents variables
-// var j = 23;
-// for (var j = 0; j < 5; j++) {
-//     console.log(j);
-// }
-// console.log(j); // Check the final value of 'j' is 5... the for cycle changed its original value of 23
-
-// console.log('\n')
-// // ES6
-// // Here i is a different variable
-// let i = 23;
-// for (let i = 0; i < 5; i++) {
-//     console.log(i);
-// }
-// console.log(i); // Check the final value of 'i' the for cycle didn't change it. It is a different variable
-
-
-
-
-//  ES5
-var name5 = 'Jane Smith';
-var age5 = 23;
-name5 = 'Jane Miller';
-console.log(name5,age5);
+driverLicense5(true);
 
 // ES6
-const name6 = 'Jane Smith';
-let age6 = 23;
-name6 = 'Jane Miller'
-console.log(name6,age6);
+function driverLicense6(passedTest) {
+    // let is Block-Scoped
+
+    let firstName;
+    const yearOfBirth = 1990;
+
+    if (passedTest) {
+        firstName = 'John';
+    }    
+    console.log(firstName,'born in',yearOfBirth,',is now officially allowed to drive a car.');
+}
+
+driverLicense6(true);
+
+
+
+let i = 23;
+
+// The following declaration of i is completely different from the previous one (line 47)
+for (let i=0; i<5; i++) {
+    console.log(i);
+}
+
+console.log(i);
