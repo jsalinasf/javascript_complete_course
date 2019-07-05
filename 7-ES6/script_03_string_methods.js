d0edae776b6492a6219084f956530ce44b821251
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-07-05 13:15:18 -0500

    ES6 Strings - back tick and startswith, includes and endswith methods

--- 7-ES6/script_03_string_methods.js
+++ 7-ES6/script_03_string_methods.js
@@ -1,2 +1,25 @@
 // Lecture 03: Strings Methods
 
+let firstName = 'John';
+let lastName = 'Smith'
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2019 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today He is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today He is ${calcAge(yearOfBirth)} years old`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n);
+
+// startsWith, includes and endsWith are case-sensitive
+console.log(n.startsWith('Jo')); // true
+console.log(n.includes('oh')); // true
+console.log(n.endsWith('nas')); // false
+
+console.log(firstName.repeat(5));
