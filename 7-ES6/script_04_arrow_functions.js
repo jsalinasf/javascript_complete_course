39d47ab4f90d018dd200f7c42d4239928470359b
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-07-05 13:31:07 -0500

    ES6 Arrow Functions - parameters, one line of code and multiple lines of code

--- 7-ES6/script_04_arrow_functions.js
+++ 7-ES6/script_04_arrow_functions.js
@@ -1,2 +1,23 @@
 // Lecture 04: Arrow Functions
 
+const years = [1990, 1965, 1982, 1937]
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2019 - el;
+})
+console.log(ages5);
+
+// ES6
+let ages6 = years.map(el => 2019 - el)
+console.log(ages6);
+
+ages6 = years.map((el, idx) => `Age ${idx + 1}: ${2019 - el}`);
+console.log(ages6);
+
+ages6 = years.map((el, idx) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age ${idx + 1}: ${age}`;
+});
+console.log(ages6);
