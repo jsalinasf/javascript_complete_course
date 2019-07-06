7cce86089c560cfd68caebabb736e73e1b040a6e
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-07-06 15:03:56 -0500

    ES6 Spread Operators - finished - spread operator will create arrays fron node lists

--- 7-ES6/script_08_spread_operator.js
+++ 7-ES6/script_08_spread_operator.js
@@ -1,2 +1,33 @@
 // Lesson 8: Spread Operator
 
+function addFourAges(a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+
+console.log(sum1);
+
+// ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+
+const bigFamily = [...familySmith, ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+
+const all = [h,...boxes];
+
+all.forEach(cur => cur.style.color = 'purple');
